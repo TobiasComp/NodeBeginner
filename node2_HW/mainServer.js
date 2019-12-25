d41069086c722d78a8f4c2468630152bd304e7ab
TobiasComp <tuvia1234@gmail.com>
2019-12-25 15:32:54 +0200

    wednesday

--- node2_HW/mainServer.js
+++ node2_HW/mainServer.js
@@ -3,15 +3,36 @@ var mysql = require('mysql');
 var url = require('url');
 var serveFile = require('./serveFile');
 
-/* var con = mysql.createConnection({
+var con = mysql.createConnection({
     host: "127.0.0.1",
     user: "Tuvia",
     password: "",
-    database: ""
+    database: "users",
+    port: 3306
 })
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-}); */
+http.createServer(function(req,res){
+    console.log("The server got the request");
+    if (req.url == "/tuvia/"){
+        serveFile.getFile('index.html', function(data){            
+            res.end(data);
+            
+            
+        })
+    } else if (req.url == "/tuvia/api/users") {
+        var sql = 'SELECT * FROM `users.users_table` '
+        con.connect(function(err){
+            if (err) res.end(err)
+            
+        })
+        con.query(sql, function (err, result){
+            if (err) res.end(err)
+            let json = JSON.stringify(result);
+            res.end(json);
+
+        });
+
+    } else
+        res.end(req.url);
+}).listen(8080);
 
